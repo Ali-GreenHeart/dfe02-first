cec6232671dda0935d181e6b19fc7e4c16009eaa
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-12-04 16:30:17 +0400

    add axios map

--- src/Users.jsx
+++ src/Users.jsx
@@ -5,44 +5,27 @@ const url = 'https://jsonplaceholder.typicode.com/users'
 
 const Users = () => {
     const [users, setUsers] = useState([])
-    // 1 defe -> component yarananda!
-    // life-cycle -> heyat donemleri
-
 
     useEffect(() => {
-        console.log('komp yarandi!')
-        return () => {
-            console.log('komp rip!')
-        }
+        axios.get(url)
+            .then(({ data }) => {
+                setUsers(data);
+            })
     }, [])
-
-    // hook
-    // [] -> komponentin doguldugu an! mount
-    // useEffect(() => {
-    //     axios.get(url)
-    //         .then(({ data }) => {
-    //             setUsers(data);
-    //             console.log(data)
-    //         })
-    // }, [])
-
-
-    // useEffect(() => {
-    //     console.log('users deyisdi')
-    //     localStorage.setItem('users', JSON.stringify(users))
-    // }, [users])
-    // update hali -> users deyisende!
-    // useEffect(() => { }, [users, test])
-
 
     return (
         <div>
-            Users
-            <button
-                onClick={() => {
-                    a()
-                }}
-            >click me</button>
+            {
+                users.map((user) => {
+                    return (
+                        <div key={user.id}>
+                            <p>{user.name}</p>
+                            <p>{user.username}</p>
+                            <hr />
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
