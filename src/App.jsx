7d83c0129dd3e3f05841c5a9baea7a68474c5a96
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-12-11 15:33:40 +0400

    pre updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
-import Counter from "./Counter";
-import Users from "./Users";
-
-
-
 function App() {
-    const [isShown, setIsShown] = useState(true)
 
     return (
         <>
-            <button onClick={() => setIsShown(!isShown)}>show hide </button>
-            {
-                isShown && <Users />
-            }
+
         </>
     )
 }
